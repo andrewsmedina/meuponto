b964d75aa65f76ab2e1fa06699387a1749f19c92
Andrews Medina <andrewsmedina@gmail.com>
2009-10-20 11:03:33 -0200

    initial django project

---
+++ ponto/settings.py
@@ -0,0 +1,54 @@
+# Django settings for ponto project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (('Andrews Medina', 'andrewsmedina@gmail.com'),)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'banco.db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+TIME_ZONE = 'America/Chicago'
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+MEDIA_ROOT = ''
+
+MEDIA_URL = ''
+
+ADMIN_MEDIA_PREFIX = '/media/'
+
+SECRET_KEY = 's(m=o5x1kv4(99tah6v$yo(vr2_$!(hqffver4zjq-=y@$7hw%'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'ponto.urls'
+
+TEMPLATE_DIRS = (
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+)
